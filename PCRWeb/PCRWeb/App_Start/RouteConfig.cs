eb9df21dfaf69e4d46681d474ea61a03f0beeb19
anderson40205 <64389342+anderson40205@users.noreply.github.com>
2021-01-16 22:47:52 +0800

    20210106修改

--- PCRWeb/PCRWeb/App_Start/RouteConfig.cs
+++ PCRWeb/PCRWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PCRWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PrincessConnect", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
